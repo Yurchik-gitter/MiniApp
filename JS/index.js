1261b961b4aac2857d9cbc6a3d42b4177b976b93
Yurchik-gitter <kirillyurikov13@gmail.com>
2025-07-22 16:41:53 +0300

    modified:   CSS/main.css 	modified:   JS/index.js 	modified:   index.html

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,17 @@
-function changeText(id) { 
-    id.innerHTML = "Ой!";
+function changeText() { 
+    alert("gdfdfg");
 }
+
+let windowWidth = window.innerWidth + "px";
+let windowHeight = window.innerHeight + "px";
+let clientWidth = document.documentElement.clientWidth + "px";
+let clientHeight = document.documentElement.clientHeight + "px";
+
+console.log("Ширина окна (с полосой прокрутки): " + windowWidth);
+console.log("Высота окна (с полосой прокрутки): " + windowHeight);
+console.log("Ширина окна (без полосы прокрутки): " + clientWidth);
+console.log("Высота окна (без полосы прокрутки): " + clientHeight);
+
+document.body.style.maxWidth = clientWidth;
+
+window.addEventListener("resize", )
