911e56be101654a30ff8ce43fc957e5823affcdc
Yurchik-gitter <kirillyurikov13@gmail.com>
2025-07-25 19:41:03 +0300

    modified:   CSS/main.css 	new file:   PHP/.idea/.gitignore 	new file:   PHP/.idea/PHP.iml 	new file:   PHP/.idea/modules.xml 	new file:   PHP/.idea/php.xml 	new file:   PHP/.idea/vcs.xml 	modified:   PHP/CSS/auth.css 	modified:   PHP/CSS/main.css 	modified:   PHP/actions/auth_post.php 	new file:   PHP/actions/test.txt 	new file:   PHP/actions/text.txt 	new file:   PHP/actions/users.txt 	modified:   PHP/auth.php 	modified:   PHP/blocks/footer.php 	modified:   PHP/blocks/header.php 	modified:   PHP/index.php 	modified:   index.html

---
+++ PHP/actions/auth_post.php
@@ -0,0 +1,27 @@
+<?php
+$user = $_POST;
+$user['pass'] = md5($user['pass']);
+$user['repass'] = md5($user['repass']);
+
+
+//$filename = "text.txt";
+//$file = fopen($filename, mode: "w");
+//fwrite($file, "\nНовый пользователь:\n ");
+//foreach ($user as $key => $value) {
+//    echo $key.": ". $value. "<br>";
+//    fwrite($file, $key.": ". $value. "\n");
+//}
+//echo "<br>".$file;
+//fclose($file);
+
+
+$message = "";
+foreach ($user as $key => $value) {
+     $message = "$message $key: $value\r\n";}
+echo "<pre>".$message."</pre>";
+
+$to = "kirillyurikov13@gmail.com";
+$from = "kirillyurikov8@gmail.com";
+$subject = "=?uth-8?B?".base64_encode("Тема проверки")."?=";
+
+mail($to, $subject, $message, $from);
